74e7fb34f064927c9b1f06738f4c1db899a339d1
madeleine-tod <madeleine.tod@ibm.com>
2019-03-29 10:18:02 +0000

    First iteration of playing against the q player

--- BasicQPlayer.py
+++ BasicQPlayer.py
@@ -15,12 +15,12 @@
 # limitations under the License.
 # =============================================================================
 import logging
-
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-class QuantumPlayer:
+class BasicQPlayer:
     def __init__(self):
         print("made a q player")
 
@@ -32,9 +32,141 @@ class QuantumPlayer:
             int : the index of the move to be made
         """
 
-        i = 0
-        while board[i]:
-            i+=1
+        # TODO would it be better to only map to qubits that could be moves
+        # However this would mean lots of index/physical qubit number swaps
 
-        # make the next avaliable move
-        return i
+        num_qubits = 9
+        q = QuantumRegister(num_qubits)
+        c = ClassicalRegister(num_qubits)
+        qc = QuantumCircuit(q, c)
+
+        # make the option definitely a 1 if there is a move in the space already
+        for index, move in enumerate(board):
+            if move:
+                qc.x(q[index])
+                print('MY BOY X on qubit', index)
+            else :
+                # this space is a potential move
+                # so put into a superposition
+                qc.h(q[index])
+
+                t_count = 0
+
+                # so it is the start of a row
+                if index % 3 == 0:
+                    if board[index + 1]:
+                        qc.t(q[index])
+                        t_count += 1
+                    if board[index + 2]:
+                        qc.t(q[index])
+                        t_count += 1
+
+                # so it is the middle of a row
+                if index % 3 == 1:
+                    if board[index - 1]:
+                        qc.t(q[index])
+                        t_count += 1
+                    if board[index + 1]:
+                        qc.t(q[index])
+                        t_count += 1
+
+                # so it is the end of a row
+                if index % 3 == 2:
+                    if board[index - 1]:
+                        qc.t(q[index])
+                        t_count += 1
+                    if board[index - 2]:
+                        qc.t(q[index])
+                        t_count += 1
+
+                # so is the top row
+                if index / 3 < 1:
+                    if board[index + 3]:
+                        qc.t(q[index])
+                        t_count += 1
+                    if board[index + 3]:
+                        qc.t(q[index])
+                        t_count += 1
+
+                # so it is the middle row
+                if 2 > index / 3 >= 1:
+                    if board[index - 1]:
+                        qc.t(q[index])
+                        t_count += 1
+                    if board[index + 1]:
+                        qc.t(q[index])
+                        t_count += 1
+
+                # so it is the top row
+                if index / 3 >= 2:
+                    if board[index - 1]:
+                        qc.t(q[index])
+                        t_count += 1
+                    if board[index - 2]:
+                        qc.t(q[index])
+                        t_count += 1
+
+                print("applied ", t_count,  " t gates to ", index)
+
+        # hard code in the diagonals
+        if board[0] and board[0] == board[4]:
+            qc.t(q[0])
+            qc.t(q[4])
+            print('extra t gate for 0 and 4')
+        if board[0] and board[0] == board[8]:
+            qc.t(q[0])
+            qc.t(q[8])
+            print('extra t gate for 0 and 8')
+        if board[4] and board[4] == board[8]:
+            qc.t(q[4])
+            qc.t(q[8])
+            print('extra t gate for 4 and 8')
+        if board[2] and board[2] == board[4]:
+            qc.t(q[2])
+            qc.t(q[4])
+            print('extra t gate for 2 and 4')
+        if board[2] and board[2] == board[6]:
+            qc.t(q[2])
+            qc.t(q[6])
+            print('extra t gate for 2 and 6')
+        if board[4] and board[4] == board[6]:
+            qc.t(q[4])
+            qc.t(q[6])
+            print('extra t gate for 4 and 6')
+
+        for index, move in enumerate(board):
+            if not move:
+                qc.h(q[index])
+        qc.measure(q, c)
+
+        backend = Aer.get_backend('qasm_simulator')
+        print(qc)
+        shots = 100
+        job_sim = execute(qc, backend, shots=shots)
+        sim_result = job_sim.result().get_counts(qc)
+
+        counts = [0]*num_qubits
+
+        for key, count in sim_result.items():
+            # need to iterate over the results and see when the value was chosen most
+
+            #keys are the opposite way round to expected
+            key = key[::-1]
+            print(key, " : ", count)
+            for index, val in enumerate(key):
+                if val == '1':
+                    counts[index] += count
+
+
+        for i, c in enumerate(counts):
+            print(i, " - ", c, ' occupied? ', board[i])
+
+        max_count = 0
+        max_index = 0
+        for index, count in enumerate(counts):
+            if not board[index] and count > max_count:
+                max_index = index
+                max_count = count
+
+        print("move : ",max_index)
+        return max_index
